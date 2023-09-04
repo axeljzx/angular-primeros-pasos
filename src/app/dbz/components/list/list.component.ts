119b3a8d37af32d554095f586dff8fda2841dbfa
Axel <axel_553m@hotmail.com>
2023-09-04 17:47:29 -0500

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+
+  @Input()
+  public characterList: Character[] = [{
+    id:'',
+    name: 'Trunk',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id:string):void{
+      this.onDelete.emit(id);
+  }
+
+}
